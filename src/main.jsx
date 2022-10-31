0f149f4dcca23bb536c5efb4fc2414e58890cd97
Jörg Oertel <tyskerjoerg@gmail.com>
2022-10-31 13:35:17 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import './styles/main.css'
 import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx"
+import Search from "./components/Search.jsx";
+import Contact from "./components/Contact.jsx";
+import Login from "./components/Login.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <Header/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="search" element={<Search/>}/>
+                <Route
+                    path="contact"
+                    element={<Contact
+                        name={"Jörg Oertel"}
+                        address={{street: "Baunehøjvej 6", town: "Lyngby", country: "Denmark"}}/>}/>
+                <Route path="login" element={<Login/>}/>
+            </Routes>
         </BrowserRouter>
     </React.StrictMode>
 )
