22a021c11a4d8ef1013b62a2d6e549051b7ef2c1
rasm445f <90387951+rasm445f@users.noreply.github.com>
2022-11-10 23:33:46 +0100

    dont think about it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {useEffect,useState} from "react";
 
 function Home(props) {
+const [intiailState, setIntiailState] = useState({});
 
+useEffect(() => {
+    fetch("http://localhost:8080/ca2_backend_war_exploded/api/info/all").then(res =>{
+        if(res.ok){
+            return res.json()
+        }
+    }).then(jsonResponse => setIntiailState(jsonResponse))
+
+}, [])
+
+    console.log(intiailState)
     return (
         <div>
+            {intiailState.length > 0 && intiailState.map((e,i) => <li key={i}>{e}</li>)}
             <h3>Homepage</h3>
         </div>
     );
